23bdf4893ff33a0b6e4e0404aae201c19d5759f2
Edilberto <edi_vazquez12@hotmail.com>
2021-08-13 05:33:22 -0500

    character-card styles

--- src/components/character-card.ts
+++ src/components/character-card.ts
@@ -1,11 +1,13 @@
+// Class to define component attributes
 class Properties {
   name: string = "";
   species: string = "";
   image: string = "";
 }
 
+// Class to define the base component of the component
 abstract class BaseComponent<Properties> extends HTMLElement {
-  public static _keys: any;
+  private static _keys: any;
   private _data: Properties;
 
   constructor() {
@@ -13,30 +15,87 @@ abstract class BaseComponent<Properties> extends HTMLElement {
     this._data = {} as Properties;
     this.attachShadow({ mode: "open" });
   }
-  public get data() {
+
+  public get data(): Properties {
     return this._data;
   }
-  static get observedAttributes() {
-    return (this.constructor as any)._keys || [];
+
+  public abstract getTemplate(): HTMLTemplateElement;
+
+  public render(): void {
+    this.shadowRoot?.appendChild(this.getTemplate().content.cloneNode(true));
   }
-  attributeChangedCallback(name: string, oldValue: any, newValue: any) {
-    if (oldValue !== newValue) {
-      (this.data as any)[name] = newValue;
-    }
+
+  connectedCallback(): void {
     this.render();
   }
 
-  public abstract render(): void;
+  static get observedAttributes(): any {
+    return (this.constructor as any)._keys || [];
+  }
+
+  attributeChangedCallback(name: string, oldValue: any, newValue: any): void {
+    if (oldValue !== newValue) {
+      (this.data as any)[name] = newValue;
+    }
+    this.getTemplate();
+  }
 }
 
+// Class to create the template
 @Observes(Properties)
 class CharacterCard extends BaseComponent<Properties> {
   constructor() {
     super();
   }
 
-  render() {
-    let template = document.createElement("template");
+  getStyles(): string {
+    return `
+      <style>
+        * {
+          box-sizing: border-box;
+          margin: 0;
+          padding: 0;
+          font-size: 62.5%;
+        }
+        .character-card {
+          width: 300px;
+          height: 380px;
+          background: #fcfcfc;
+          border-radius: 10px;
+          box-shadow: 0px 3px 6px 0px rgba(0, 0, 0, 0.16);
+        }
+        .character-card a {
+          width: 100%
+          height: 100%;
+          display: grid;
+          grid-template-rows: 300px 80px;
+          text-decoration: none;
+          color: black;
+        }
+        .character-card a img {
+          width: 300px;
+          height: auto;
+          border-top-left-radius: 10px;
+          border-top-right-radius: 10px;
+        }
+        .character-card a div {
+          width: 100%;
+          height: 100%;
+          padding: 0 14px;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+        }
+        .character-card a div span {
+          font-size: 1.4rem;
+        }
+      </style>
+    `;
+  }
+
+  getTemplate(): HTMLTemplateElement {
+    const template: HTMLTemplateElement = document.createElement("template");
     template.innerHTML = `
       <article class="character-card">
         <a href="#/1/">
@@ -47,21 +106,17 @@ class CharacterCard extends BaseComponent<Properties> {
           </div>
         </a>
       </article>
+      ${this.getStyles()}
     `;
     return template;
-  }
-
-  connectedCallback() {
-    this.shadowRoot?.appendChild(this.render().content.cloneNode(true));
   }
 }
 
 type Type = new (...args: any[]) => Properties;
 function Observes<Properties extends {}>(type: Type) {
-  // the original decorator
   function internal(target: Object): void {
     Object.defineProperty(target.constructor, "_keys", {
-      get: function () {
+      get: function (): string[] {
         const defaults = new type();
         return Object.keys(defaults);
       },
